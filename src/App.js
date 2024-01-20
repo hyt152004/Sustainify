3bcd52f3a658fc1c4c46116ad8cbbb13a3c9e4c2
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 12:58:52 -0800

    asd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn David
+          Learn Leticiaa hello
         </a>
       </header>
     </div>
