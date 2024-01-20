4d0cc4e0b548bc2c6b10e51781b9ad132a0483be
hyt152004@gmail.com <hyt152004@gmail.com>
2024-01-20 12:55:10 -0800

    first branch commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn David
+          Learn David Hong
         </a>
       </header>
     </div>
