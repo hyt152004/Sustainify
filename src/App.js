484c33ff6f511216dcb0c32fcd7b6fe118a9718c
dawon020411 <dawon020411@gmail.com>
2024-01-20 15:30:34 -0800

    basic form

--- src/App.js
+++ src/App.js
@@ -6,24 +6,6 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Dayna hello
-        </a>
-      </header>
-    </div>
-=======
     <Router>
     <div>
       <Navbar/>
@@ -35,7 +17,6 @@ function App() {
       </div>
     </div>
     </Router>
->>>>>>> 460919a8f74de827e5bda59dd133ae1a616f4b83
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import logo from '../images/logo.png'
+import './navbar.css'
 
 function Navbar() {
     return (
