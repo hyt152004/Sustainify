9b2b8d18d737127c5833306c9adadee3077ca434
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 15:03:36 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Leticiaa hello
+          Learn Leticiaa 
         </a>
       </header>
     </div>
