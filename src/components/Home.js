728a291741a46e06beab5988342034c95b0709df
hyt152004@gmail.com <hyt152004@gmail.com>
2024-01-20 14:54:18 -0800

    created home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import tempImage from "../assets/tempImage.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Sustainify</h1>
+      <h2>Learn more</h2>
+      <img src={tempImage} alt="React Image" />
+    </div>
+  );
 }
 
 export default Home;
