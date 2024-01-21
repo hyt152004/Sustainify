0ab0392269398f793a13e6a1836c6d2094313901
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 19:25:00 -0800

    fixed navbar 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navb() {
 <Navbar bg="light" data-bs-theme="light">
 <Container class="d-flex justify-content-between align-items-center">
 <Link to="/home" className="site-title me-5">
-  <Navbar.Brand href="#">
+  <Navbar.Brand href="#logo">
     <img
       className="logo"
       src={logo}
@@ -43,6 +43,9 @@ function Navb() {
     </Link>
     <Link to="/about">
     <Nav.Link href="#about" className="aboutUs ps-5">About Us</Nav.Link>
+    </Link>
+    <Link to="/main">
+    <Nav.Link href="#main " className="main ps-5">Main</Nav.Link>
     </Link>
   </Nav>
 </Container>
