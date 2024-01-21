63e79f6917f431e1f8eef34d54dcf513c11bf963
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 17:23:54 -0800

    Nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,30 @@ import React from "react";
 import {Link} from "react-router-dom"
 import logo from '../images/logo.png'
 import './navbar.css'; 
-function Navbar() {
-    return (
-        <nav className="nav">
-        <Link to="/home" className="site-title"><img src={logo} alt="logo" className="profile" /></Link>
-        <ul>
-            <li>
-                <Link to= "/home">home</Link>
-            </li>
-            <li>
-                <Link to= "/about">about</Link>
-            </li>
-        </ul>
-    </nav>
-    );
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#">
+          <img class = "logo "
+            src= {logo}
+            alt="Your Logo"
+            height= "50" // You can adjust the height according to your design
+            className="d-inline-block align-top me-3"
+          />
+          </Navbar.Brand>
+          <Nav className="me-auto home">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about" class="aboutus">About Us</Nav.Link>
+           
+          </Nav>
+        </Container>
+      </Navbar>
+  );
 }
-export default Navbar;
+
+export default ColorSchemesExample;
