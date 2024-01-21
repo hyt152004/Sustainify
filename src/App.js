1eaa6eae1cf1bf593558c4456cbe5b91bd623d9e
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 16:24:55 -0800

    bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
