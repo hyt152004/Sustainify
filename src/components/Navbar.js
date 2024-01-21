93e96e48ebb9b956591cbba83a5a4758f1750ba9
leticiascudeller <leticiascudellerc@gmail.com>
2024-01-20 18:53:48 -0800

    fix navbar bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,27 +6,31 @@ import './navbar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function ColorSchemesExample() {
+function NavBar() {
   return (
-      <Navbar bg="light" data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="#">
-          <img class = "logo "
-            src= {logo}
+    <Navbar bg="light" data-bs-theme="light" class="navbar navbar-expand-lg">
+    <Container>
+      <Link class="navbar-brand" href="#logo">
+      <img
+            src={logo}
             alt="Your Logo"
-            height= "50" // You can adjust the height according to your design
-            className="d-inline-block align-top me-3"
+            height="50" // You can adjust the height according to your design
+            className="d-inline-block align-top"
           />
-          </Navbar.Brand>
-          <Nav className="me-auto home">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about" class="aboutus">About Us</Nav.Link>
-           
-          </Nav>
-        </Container>
-      </Navbar>
+      </Link>
+      <Nav id="navigation-items" className="navbar-nav me-auto">
+        <Nav.Item>
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#about">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  </Navbar>
   );
 }
 
-export default ColorSchemesExample;
+export default NavBar;
