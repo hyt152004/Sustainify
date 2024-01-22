000c0c0c3319dea459bb73b6fde0828ae752bcda
hyt152004@gmail.com <hyt152004@gmail.com>
2024-01-22 08:59:17 -0800

    added background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,14 @@ function Navbar() {
       </Link>
       <ul>
         <li>
-          <Link to="/home">Home</Link>
+          <Link to="/home" className="link">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" className="link">
+            About
+          </Link>
         </li>
       </ul>
     </nav>
